77d77c070dd72373a18adc7f66a74a4e76272e83
Ameer <ameerapex496@gmail.com>
2023-11-18 09:46:10 +0100

    finished project logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,45 @@
-console.log("start of project");
+// getting our elements
+var progress = document.getElementById("progress");
+var prev = document.getElementById("prev");
+var nextBtn = document.getElementById("next");
+var cercles = document.querySelectorAll(".cercle");
+// creating global varibales
+var currentActive = 1;
+// our functions
+// next func
+var nextHandler = function () {
+    currentActive++;
+    console.log("you clicked");
+    if (currentActive > cercles.length) {
+        currentActive = cercles.length;
+    }
+    console.log(currentActive);
+    update();
+};
+// prev func
+var prevHandler = function () {
+    currentActive--;
+    console.log("you clicked");
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    console.log(currentActive);
+    update();
+};
+// our update func
+var update = function () {
+    cercles.forEach(function (cercle, idx) {
+        if (idx < currentActive) {
+            cercle.classList.add("active");
+        }
+        else {
+            cercle.classList.remove("active");
+        }
+    });
+    var actives = document.querySelectorAll(".active");
+    progress.style.width =
+        ((actives.length - 1) / (cercles.length - 1)) * 100 + "%";
+};
+// our event lisnters
+nextBtn.addEventListener("click", nextHandler);
+prev.addEventListener("click", prevHandler);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,52 @@
-console.log("start of project");
+// getting our elements
+const progress: any = document.getElementById("progress");
+const prev: any = document.getElementById("prev");
+const nextBtn: any = document.getElementById("next");
+const cercles = document.querySelectorAll(".cercle");
+
+// creating global varibales
+let currentActive: number = 1;
+
+// our functions
+
+// next func
+const nextHandler = () => {
+  currentActive++;
+  console.log("you clicked");
+  if (currentActive > cercles.length) {
+    currentActive = cercles.length;
+  }
+  console.log(currentActive);
+  update();
+};
+
+// prev func
+const prevHandler = () => {
+  currentActive--;
+  console.log("you clicked");
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  console.log(currentActive);
+  update();
+};
+
+// our update func
+const update = () => {
+  cercles.forEach((cercle, idx) => {
+    if (idx < currentActive) {
+      cercle.classList.add("active");
+    } else {
+      cercle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (cercles.length - 1)) * 100 + `%`;
+};
+
+// our event lisnters
+nextBtn.addEventListener("click", nextHandler);
+prev.addEventListener("click", prevHandler);
